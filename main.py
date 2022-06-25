889809ca65a297404f78e185708dbc52b88f62a5
Wally <107321197+oramirez2025@users.noreply.github.com>
2022-06-25 16:11:19 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -24,13 +24,23 @@ def play_next_song(ctx):
     #Queue is not empty 
     if len(queue) >= 1:
         newSong = queue.pop(0)
+        #The next song is an mp3 file 
         if str(newSong).endswith(".mp3"): 
             source = discord.FFmpegOpusAudio(newSong)
             asyncio.run_coroutine_threadsafe(ctx.send("Now playing..." + newSong),client.loop)
+        #The next song was either a youtube link or a search
         else: 
             source = newSong
             asyncio.run_coroutine_threadsafe(ctx.send("Now playing..." + str(source)),client.loop)
         voice.play(source,after=lambda f: play_next_song(ctx))
+
+def search_yt(item):
+    with YoutubeDL(ydlOptions) as ydl:
+        try:
+            info = ydl.extract_info("ytsearch:%s" % item,download=False)["entries"][0]
+        except:
+            return False
+    return info["formats"][0]["url"]
 
 @client.event
 async def on_ready():
@@ -60,7 +70,7 @@ async def disconnect(ctx):
 async def play(ctx,url=None):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     #If the user uploaded an audio file 
-    if not voice.is_playing() and len(queue) == 0 and url == None:
+    if not voice.is_playing() and len(queue) == 0 and not url.startswith("https://www.youtube.com") and len(ctx.message.content) == 0:
         #Checks if the user uploaded a file
         if len(ctx.message.attachments) == 0:
             await ctx.send("No file was attached/no link was provided")
@@ -71,20 +81,32 @@ async def play(ctx,url=None):
             await ctx.send("Now playing... " + filename)
             voice.play(source,after=lambda f: play_next_song(ctx))
     #If the user sent a youtube link
-    elif url != None and len(queue) == 0 and not voice.is_playing():
+    elif url.startswith("https://www.youtube.com") and len(queue) == 0 and not voice.is_playing():
         with youtube_dl.YoutubeDL(ydlOptions) as ydl:
             info = ydl.extract_info(url,download=False)
             newUrl = info['formats'][0]['url']
             source = await discord.FFmpegOpusAudio.from_probe(newUrl,**ffmpegOptions)
             await ctx.send("Now playing... " + YouTube(url).title)
             voice.play(source,after=lambda f: play_next_song(ctx))
-    elif url != None and len(queue) >= 0 and not is_paused: 
+    #If user sent a link but a song is currently playing 
+    elif url.startswith("https://www.youtube.com") and len(queue) >= 0 and not is_paused: 
         with youtube_dl.YoutubeDL(ydlOptions) as ydl:
             info = ydl.extract_info(url,download=False)
             newUrl = info['formats'][0]['url']
             source = await discord.FFmpegOpusAudio.from_probe(newUrl,**ffmpegOptions)
             queue.append(source)    
             await ctx.send(YouTube(url).title + " has been added to the queue")
+    #If a user enters the title of a song 
+    elif not url.startswith("https://www.youtube.com") and len(queue) == 0 and not voice.is_playing():
+        song = ctx.message.content[5::]
+        source = discord.FFmpegOpusAudio(search_yt(song),**ffmpegOptions)
+        voice.play(source,after=lambda f: play_next_song(ctx))
+        await ctx.send("Now playing... " + song)
+    elif not url.startswith("https://www.youtube.com") and len(queue) >= 0 and voice.is_playing():
+        song = ctx.message.content[5::]
+        source = discord.FFmpegOpusAudio(search_yt(song),**ffmpegOptions)
+        queue.append(source)    
+        await ctx.send(song + " has been added to the queue")
     #Add the audio file to the queue
     else:
         filename = ctx.message.attachments[0].filename
